49d1f1e2758deb131a6aa8c79dfc43ac7ac3acc1
painedViktor <mudritskiy.vk@gmail.com>
2024-04-28 14:23:50 +0300

    [feat] ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/core/password_validator.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  String _validationError = '';
+
+  late final TextEditingController _con;
+  final PasswordValidator _passwordValidator = PasswordValidator();
+
+  @override
+  void initState() {
+    _con = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _con.dispose();
+    super.dispose();
+  }
+
+  void _validate(String password) {
+    final res = _passwordValidator.validate(password);
+
+    if (res) {
+      _validationError = 'Correct';
+    } else {
+      _validationError = 'Incorrect';
+    }
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _con,
+                onChanged: (_) {
+                  _validationError = '';
+                  setState(() {});
+                },
+              ),
+              Text(
+                _validationError,
+                key: const Key('validation_text'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _validate(_con.text);
+                },
+                child: const Text('Validate'),
+              ),
+            ],
+          ),
         ),
       ),
     );
